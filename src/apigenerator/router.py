434420a87e878d9690488b71a425a571ef4658b4
Rashicom <rashid.kp484@gmail.com>
2025-04-18 23:52:56 +0530

    updated(apigenerator):endpoint creation api

--- src/apigenerator/router.py
+++ src/apigenerator/router.py
@@ -1,4 +1,15 @@
 from fastapi import APIRouter
-
+from authx.auth import CurrentUser
+from .schema import EndpointsRequestSchema, EndpointsResponseSchema
+from .repository import EndpointRepository
 
 router = APIRouter()
+
+router.post(
+    "/create",
+    summary="Create endpoint",
+    description="Create endpoint",
+    response_model=EndpointsResponseSchema
+)
+async def create_endpoint(user:CurrentUser, data:EndpointsRequestSchema):
+    return await EndpointRepository.create(user=user, **data.model_dump())
