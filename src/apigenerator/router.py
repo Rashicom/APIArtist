a3013ec52ac6fe9907e51785df0226972783a826
Rashicom <rashid.kp484@gmail.com>
2025-04-20 19:05:31 +0530

    update(apigenerator):api skeleton

--- src/apigenerator/router.py
+++ src/apigenerator/router.py
@@ -3,6 +3,7 @@ from authx.auth import CurrentUser
 from .schema import EndpointsRequestSchema, EndpointsResponseSchema
 from .repository import EndpointRepository
 from project.repository import ProjectRepository
+from beanie import BeanieObjectId
 
 router = APIRouter()
 
@@ -17,3 +18,41 @@ async def create_endpoint(user:CurrentUser, data:EndpointsRequestSchema):
     if not project_obj:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="project does not found")
     return await EndpointRepository.create(user=user, **data.model_dump())
+
+
+
+@router.get(
+    "/list",
+    summary="List all endpoint",
+    description="List all endpoints"
+)
+async def list_endpoint(user:CurrentUser):
+    pass
+
+
+@router.get(
+    "/{endpoint_id}/retrieve",
+    summary="Get all endpoint",
+    description="Get all endpoints"
+)
+async def get_endpoint(user:CurrentUser, id: BeanieObjectId):
+    pass
+
+
+@router.patch(
+    "/{endpoint_id}/update",
+    summary="Update endpoint",
+    description="Update endpoint"
+)
+async def update_endpoint(user:CurrentUser, id: BeanieObjectId):
+    pass
+
+
+@router.delete(
+    "/{endpoint_id}/delete",
+    summary="Delete endpoint",
+    description="Delete endpoint"
+)
+async def delete_endpoint(user:CurrentUser, id: BeanieObjectId):
+    pass
+
