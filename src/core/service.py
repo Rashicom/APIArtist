950cec5baa795cdc7bb32a3fa51a5596521ee4a7
Rashicom <rashid.kp484@gmail.com>
2025-04-23 23:37:39 +0530

    update(core):get_endpoint

--- src/core/service.py
+++ src/core/service.py
@@ -7,3 +7,15 @@ from authx.models import User
 
 async def get_project_by_id(user:User,project_id: BeanieObjectId):
     project_obj = ProjectRepository.retrieve_project(user,project_id)
+
+
+async def get_endpoint():
+    """
+    user:user
+    endpoint:endpoint string
+    TODO: find endpoint obj using endpoint which is provided in url
+        endpoint_obj = /api/user/{user_id}/change
+        endpoint_str = /api/user/12/change
+    """
+
+    pass
