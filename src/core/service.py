11f7f12b1bb2117cae10ec3cf59aef35146f88f0
Rashicom <rashid.kp484@gmail.com>
2025-04-25 22:00:38 +0530

    update(core):core documented

--- src/core/service.py
+++ src/core/service.py
@@ -23,23 +23,42 @@ class EndpointManager:
         self.end_point_string = end_point
         self.end_point_obj = None
 
-    async def get_end_point(self):
-        "get end point"
+    async def validate_end_point(self):
+        """
+        Validate endpoint : check the endpoint is exist in db or not
+            - if yes set self.end_point_obj
+            - else rise http exception
+        """
         pass
 
     async def get_available_methods(self):
+        """
+        Get all avalable method for the self.end_point_obj
+            - dependency >> validate_end_point must be called before this
+        """
         pass
 
     async def get_endpoint_type(self):
+        """
+        Get endpoit type(static, dynamic)
+            - dependency >> validate_end_point must be called before this
+        """
         pass
 
     async def get_data(self):
         """
         Return data from endpoint
+            - call self.__get
+            - dependency >> validate_end_point must be called before this
         """
         pass
 
-    async def set_data(self):
+    async def set_data(self, method, data):
+        """
+        set data to an endpoint
+            - call self.__<method>
+            - dependency >> validate_end_point must be called before this
+        """
         pass
 
     async def __post(self):
