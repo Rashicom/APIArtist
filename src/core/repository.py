c5ac95afdf8fe0d45100f4526ea482c326d77d45
Rashicom <rashid.kp484@gmail.com>
2025-04-28 22:38:53 +0530

    update(core):endpoint repository updated

--- src/core/repository.py
+++ src/core/repository.py
@@ -1,14 +1,19 @@
 from project.models import Project
 from apigenerator.models import Endpoints
 from authx.models import User
+from beanie import BeanieObjectId
 
 
 class CoreRepository:
-    async def get_endpoints_by_project_id(self):
+    async def get_endpoints_by_project_id(
+        self, user_id: BeanieObjectId, project_id: BeanieObjectId
+    ):
         """
         return all endpoints of a project
         """
-        pass
+        return await Endpoints.find(
+            Endpoints.user.id == user_id, Endpoints.project.id == project_id
+        ).to_list()
 
     async def get_endpoint_by_id(self):
         """
